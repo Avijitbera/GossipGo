b80af9e6f46eec0bca69ede831e275bc1832cd2b
Avijitbera <avijitbera775@gmail.com>
2025-02-16 23:41:04 +0530

    created resource_from_claims function

--- lib/gossip_go/accounts/guardian.ex
+++ lib/gossip_go/accounts/guardian.ex
@@ -7,4 +7,12 @@ defmodule GossipGo.Accounts.Guardian do
     {:ok, to_string(user.user_id)}
   end
 
+  def resource_from_claims(claims) do
+    user_id = claims["sub"]
+    user = Accounts.get_user!(user_id)
+    {:ok, user}
+  rescue
+    Ecto.NoResultsError -> {:error, :resource_not_found}
+  end
+
 end
