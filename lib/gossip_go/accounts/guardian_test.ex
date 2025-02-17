b1e285c219cdbbb2429b6316f852b132a0e749a9
Avijitbera <avijitbera775@gmail.com>
2025-02-17 23:19:57 +0530

    created test for resource from claims

--- lib/gossip_go/accounts/guardian_test.ex
+++ lib/gossip_go/accounts/guardian_test.ex
@@ -16,5 +16,17 @@ defmodule GossipGo.Accounts.GuardianTest do
       assert {:ok, user_id} = Guardian.subject_for_token(user, %{})
       assert user_id == to_string(user.user_id)
     end
+    test "resource_from_claims/1 returns user from claims" do
+      user_attrs = %{
+        username: "testuser",
+        email: "test@example.com",
+        password: "password123"
+      }
+
+      {:ok, user} = Accounts.register_user(user_attrs)
+      claims = %{"sub" => to_string(user.user_id)}
+      assert {:ok, resource} = Guardian.resource_from_claims(claims)
+      assert resource.user_id == user.user_id
+    end
   end
 end
