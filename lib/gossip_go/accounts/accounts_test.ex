6cc43d5b8d2f2fe50b100aea07354d7ab193b6d7
Avijitbera <avijitbera775@gmail.com>
2025-02-17 22:40:06 +0530

    create test for user2

--- lib/gossip_go/accounts/accounts_test.ex
+++ lib/gossip_go/accounts/accounts_test.ex
@@ -15,6 +15,18 @@ defmodule GossipGo.Accounts.AccountsTest do
       assert user.email == "test@example.com"
     end
 
+    test "authenticate_user/2 authenticates the user" do
+      attrs = %{
+        "username" => "testuser",
+        "email" => "test@mail.com",
+        "password" => "password123"
+      }
+      {:ok, user} = Accounts.register_user(attrs)
+      assert {:ok, authenticated_user} = Accounts.authenticate_user(user.email, attrs["password"])
+
+      assert authenticated_user.user_id == user.user_id
+    end
+
   end
 
 end
